d33c5898d8750a13ce1d1f9797959d4aabc60f46
ShawnDordeJay <igor.jerinic@hotmail.com>
2020-10-07 08:02:07 +0200

    add sunrise/sunset

--- init_watch.py
+++ init_watch.py
@@ -6,34 +6,75 @@ from datetime import datetime as DATETIME
 import time
 import atexit
 
+# global variables
 path_gif = '/home/pi/gifs/'
 path_imageviewer = '/home/pi/DigitalLEDWatch/led-image-viewer'
 start_service = 'systemctl start watch.service'
 stop_service = 'systemctl stop watch.service'
+brightness_std = 50
+brightness_sunset = 8
+# x is for gif -> just show gif once per minute and not in loop
+x = 0
+temp_minute = 0
 
 # load database
 mydb = mysql.connector.connect(
     host='localhost',
     user='pi',
-    password='',
+    password='<enter your password>',
     database='watch'
 )
 
 mycursor = mydb.cursor()
-sql = 'select * from gifs'
-mycursor.execute(sql)
-whole_table = mycursor.fetchall()
+table_gif = 'select * from gifs'
+mycursor.execute(table_gif)
+gifs = mycursor.fetchall()
 
-# start watch service
+table_sunrise_sunset = 'select * from sunrise_sunset'
+mycursor.execute(table_sunrise_sunset)
+sunrise_sunset = mycursor.fetchall()
+
+# functions
+def change_brightness(brightness):
+    configfile = '/etc/watch_args'
+
+    with open(configfile, "w") as f:
+        f.write('ARG1=-b {}'.format(brightness))
+
+def start_stop_gif(gifname):
+    os.system(stop_service)
+    os.system('{} -t15 {}{}'.format(path_imageviewer, path_gif, gifname))
+    os.system(start_service)
+    temp_minute = DATETIME.now().minute
+    x = 1
+
+# start watch service once
 os.system(start_service)
 
-# x is for gif -> just show gif once per minute and not in loop
-x = 0
-temp_minute = 0
-
 while 1:
+
+    for row in sunrise_sunset:
+
+        entry_year = row[0].year
+        entry_month = row[0].month
+        entry_day = row[0].day
+        entry_hour_sunrise = row[1].seconds // 3600
+        entry_minute_sunrise = (row[1].seconds % 3600) // 60
+        entry_hour_sunset = row[2].seconds // 3600
+        entry_minute_sunset = (row[2].seconds % 3600) // 60
+
+	#sunrise
+        if entry_year == DATETIME.now().year and entry_month == DATETIME.now().month and entry_day == DATETIME.now().day and entry_hour_sunrise == DATETIME.now().hour and entry_minute_sunrise == DATETIME.now().minute and x == 0:
+            change_brightness(brightness_std)
+            start_stop_gif('sunrise.gif')
+
+	#sunset
+        if entry_year == DATETIME.now().year and entry_month == DATETIME.now().month and entry_day == DATETIME.now().day and entry_hour_sunset == DATETIME.now().hour and entry_minute_sunset == DATETIME.now().minute and x == 0:
+            change_brightness(brightness_sunset)
+            start_stop_gif('sunset.gif')
+
     # search for entries in database and corelate them with time
-    for row in whole_table:
+    for row in gifs:
 
         # check if everyday (date == NULL) or one a year (9999-month-day) or a special day (2021-08-19)
         # everyday -> date == NULL/None
@@ -45,11 +86,7 @@ while 1:
             entry_second = row[1].seconds % 60
 
             if DATETIME.now().hour == entry_hour and DATETIME.now().minute == entry_minute and x == 0:
-                os.system(stop_service)
-                os.system('{} -t15 {}{}'.format(path_imageviewer, path_gif, row[2]))
-                os.system(start_service)
-                temp_minute = DATETIME.now().minute
-                x = 1
+                start_stop_gif(row[2])
 
         else:
 
@@ -64,29 +101,19 @@ while 1:
             # every year on this date, display the gif every full hour e.g birthday
             if entry_year == 9999 and entry_month == DATETIME.now().month and entry_day == DATETIME.now().day and entry_hour == 0 and entry_minute == 0 and x == 0:
                 if DATETIME.now().minute == 0:
-                    os.system(stop_service)
-                    os.system('{} -t15 {}{}'.format(path_imageviewer, path_gif, row[2]))
-                    os.system(start_service)
-                    temp_minute = DATETIME.now().minute
-                    x = 1
+                    start_stop_gif(row[2])
+
             # every year on this date, display once on defined time e.g
             elif entry_year == 9999 and entry_month == DATETIME.now().month and entry_day == DATETIME.now().day and entry_hour == DATETIME.now().hour and entry_minute == DATETIME.now().minute and x == 0:
-                os.system(stop_service)
-                os.system('{} -t15 {}{}'.format(path_imageviewer, path_gif, row[2]))
-                os.system(start_service)
-                temp_minute = DATETIME.now().minute
-                x = 1
+               start_stop_gif(row[2])
+
             # defined date and time
             elif entry_year == DATETIME.now().year and entry_month == DATETIME.now().month and entry_day == DATETIME.now().day and entry_hour == DATETIME.now().hour and entry_minute == DATETIME.now().minute and x == 0:
-                os.system(stop_service)
-                os.system('{} -t15 {}{}'.format(path_imageviewer, path_gif, row[2]))
-                os.system(start_service)
-                temp_minute = DATETIME.now().minute
-                x = 1
+                start_stop_gif(row[2])
 
         # check if gif already displayed this minute otherwise change x back to 0
         if temp_minute != DATETIME.now().minute:
             x = 0
 
         # chill a little bit till checking again
-        time.sleep(1)
+        time.sleep(0.25)
--- watch.cc
+++ watch.cc
@@ -44,57 +44,11 @@ static int usage(const char *progname)
 
   return 1;
 }
-/*
-static bool parseColor(Color *c, const char *str)
-{
-    return sscanf(str, "%hhu,%hhu,%hhu", &c->r, &c->g, &c->b) == 3;
-}
-*/
+
 static bool FullSaturation(const Color &c)
 {
   return (c.r == 0 || c.r == 255) && (c.g == 0 || c.g == 255) && (c.b == 0 || c.b == 255);
 }
-
-/*
-static int exec_prog(const char **my_argv)
-{
-
-  pid_t my_pid;
-  int status, timeout; //unused ifdef WAIT_FOR_COMPLETION
-
-  if (0 == (my_pid = fork()))
-  {
-    if (-1 == execve(my_argv[0], (char **)my_argv, NULL))
-    {
-      perror("child process execve failed [%m]");
-      return -1;
-    }
-  }
-
-#ifdef WAIT_FOR_COMPLETION
-  timeout = 1000;
-
-  while (0 == waitpid(my_pid, &status, WNOHANG))
-  {
-    if (--timeout < 0)
-    {
-      perror("timeout");
-      return -1;
-    }
-    sleep(1);
-  }
-  printf("%s WEXITSTATUS %d WIFEXIED %d [status %d]\n",
-         my_argv[0], WEXITSTATUS(status), WIFEXITED(status), status);
-
-  if (1 != WIFEXITED(status) || 0 != WEXITSTATUS(status))
-  {
-    perror("%s failed, halt system");
-    return -1;
-  }
-#endif
-  return 0;
-}
-*/
 
 int main(int argc, char *argv[])
 {
@@ -130,6 +84,14 @@ int main(int argc, char *argv[])
   int y2_orig = 32;
   int letter_spacing = 0;
   int brightness = 50;
+
+  int opt;
+  	while ((opt = getopt(argc, argv, "b:")) != -1) {
+    		switch (opt) {
+		    case 'b': brightness = atoi(optarg); break;
+		    default: break;
+		}
+	  }
 
   rgb_matrix::Font font_time;
   if (!font_time.LoadFont(bdf_font_file_time))
@@ -179,24 +141,12 @@ int main(int argc, char *argv[])
   signal(SIGTERM, InterruptHandler);
   signal(SIGINT, InterruptHandler);
 
-  int x = 0;
-  int temp_hour = 0;
-  int temp_min = 0;
-  //const char *path_imageviewer = "sudo ./home/watch/DigitalLEDWatch/led-image-viewer";
-  //const char *duration_animation = "-t10";
-  //const char *sudo = "/usr/bin/sudo";
-
   while (!interrupt_received)
   {
     localtime_r(&next_time.tv_sec, &tm);
     strftime(time_buffer, sizeof(time_buffer), time_format, &tm);
     strftime(date_buffer, sizeof(date_buffer), date_format, &tm);
     offscreen->Fill(bg_color.r, bg_color.g, bg_color.b);
-
-    if (tm.tm_hour == 10 && tm.tm_min == 42 && x == 0)
-    {
-     
-    }
 
     //get length of text to center and put color to 0 0 0
     int length = rgb_matrix::DrawText(offscreen, font_time, x1, y1,
@@ -230,11 +180,6 @@ int main(int argc, char *argv[])
     offscreen = matrix->SwapOnVSync(offscreen);
 
     next_time.tv_sec += 1;
-
-    if (tm.tm_hour != temp_hour && tm.tm_min != temp_min)
-    {
-      x = 0;
-    }
   }
 
   // Finished. Shut down the RGB matrix.
